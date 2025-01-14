3122ac8e7c8c04edba4a2f167978121715f1be39
Shuhaku <Shuhaku7@icloud.com>
2025-01-14 22:43:37 +0900

    Time: 2 ms (69.07%), Space: 41.6 MB (61.92%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int n = citations.length;
+
+        for (int i = 0; i < n; i++) {
+            int h = n - i; 
+            if (h <= citations[i]) {
+                return h;
+            }
+        }
+
+        return 0;
+    }
+}
